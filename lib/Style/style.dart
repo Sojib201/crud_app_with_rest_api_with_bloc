9dcf847c5b23c4d9368c1969888af3c4bdd40a1f
Abu Noman Basar <abunomanbasar.eappair@gmail.com>
2024-12-31 16:56:01 +0600

    dvz

--- lib/Style/style.dart
+++ lib/Style/style.dart
@@ -4,25 +4,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
+const colorRed = Color.fromRGBO(231, 28, 36, 1);
+const colorGreen = Color.fromRGBO(33, 191, 115, 1);
+const colorWhite = Color.fromRGBO(255, 255, 255, 1);
+const colorDarkBlue = Color.fromRGBO(44, 62, 80, 1);
 
-const colorRed=Color.fromRGBO(231, 28, 36, 1);
-const colorGreen=Color.fromRGBO(33, 191, 115, 1);
-const colorWhite=Color.fromRGBO(255, 255, 255, 1);
-const colorDarkBlue=Color.fromRGBO(44, 62, 80, 1);
-
-
-
-SliverGridDelegateWithFixedCrossAxisCount ProductGridViewStyle(){
+SliverGridDelegateWithFixedCrossAxisCount ProductGridViewStyle() {
   return SliverGridDelegateWithFixedCrossAxisCount(
-    crossAxisCount: 2,
-    mainAxisSpacing: 2,
-    mainAxisExtent: 250
-  );
+      crossAxisCount: 2, mainAxisSpacing: 2, mainAxisExtent: 250);
 }
 
-
-
-void ErrorToast(msg){
+void ErrorToast(msg) {
   Fluttertoast.showToast(
       msg: msg,
       toastLength: Toast.LENGTH_SHORT,
@@ -30,12 +22,10 @@ void ErrorToast(msg){
       timeInSecForIosWeb: 1,
       backgroundColor: colorRed,
       textColor: colorWhite,
-      fontSize: 16.0
-  );
+      fontSize: 16.0);
 }
 
-
-void SuccessToast(msg){
+void SuccessToast(msg) {
   Fluttertoast.showToast(
       msg: msg,
       toastLength: Toast.LENGTH_SHORT,
@@ -43,15 +33,11 @@ void SuccessToast(msg){
       timeInSecForIosWeb: 1,
       backgroundColor: colorGreen,
       textColor: colorWhite,
-      fontSize: 16.0
-  );
-
+      fontSize: 16.0);
 }
 
-
-
-SvgPicture ScreenBackground(context){
- return SvgPicture.asset(
+SvgPicture ScreenBackground(context) {
+  return SvgPicture.asset(
     'assets/images/screen-back.svg',
     alignment: Alignment.center,
     width: MediaQuery.of(context).size.width,
@@ -60,54 +46,41 @@ SvgPicture ScreenBackground(context){
   );
 }
 
-
-InputDecoration AppInputDecoration(label){
+InputDecoration AppInputDecoration(label) {
   return InputDecoration(
-    focusedBorder: const OutlineInputBorder(
-      borderSide: const BorderSide(color: colorGreen,width: 1)
-    ),
-    fillColor: colorWhite,
-    filled: true,
-    contentPadding: EdgeInsets.fromLTRB(20, 10, 10, 20),
-    enabledBorder: const OutlineInputBorder(
-        borderSide: const BorderSide(color: colorWhite,width: 0)
-    ),
-    border: OutlineInputBorder(),
-    labelText: label
-  );
+      focusedBorder: const OutlineInputBorder(
+          borderSide: const BorderSide(color: colorGreen, width: 1)),
+      fillColor: colorWhite,
+      filled: true,
+      contentPadding: EdgeInsets.fromLTRB(20, 10, 10, 20),
+      enabledBorder: const OutlineInputBorder(
+          borderSide: const BorderSide(color: colorWhite, width: 0)),
+      border: OutlineInputBorder(),
+      labelText: label);
 }
 
-
-
-
-
-DecoratedBox AppDropDownStyle(child){
+DecoratedBox AppDropDownStyle(child) {
   return DecoratedBox(
-      decoration: BoxDecoration(
-        color: colorWhite,border: Border.all(color: colorWhite,width: 1),
-        borderRadius: BorderRadius.circular(4)
-      ),
-      child: Padding(
-        padding: EdgeInsets.only(left: 30,right: 30),
-        child: child,
-      ),
+    decoration: BoxDecoration(
+        color: colorWhite,
+        border: Border.all(color: colorWhite, width: 1),
+        borderRadius: BorderRadius.circular(4)),
+    child: Padding(
+      padding: EdgeInsets.only(left: 30, right: 30),
+      child: child,
+    ),
   );
 }
 
-
-
-
-ButtonStyle AppButtonStyle(){
+ButtonStyle AppButtonStyle() {
   return ElevatedButton.styleFrom(
-    elevation: 1,
-    padding: EdgeInsets.all(0),
-    backgroundColor: Colors.transparent,
-    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(6))
-  );
+      elevation: 1,
+      padding: EdgeInsets.all(0),
+      backgroundColor: Colors.transparent,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(6)));
 }
 
-
-Ink SuccessButtonChild(ButtonText){
+Ink SuccessButtonChild(ButtonText) {
   return Ink(
     decoration: BoxDecoration(
       color: colorGreen,
@@ -116,10 +89,10 @@ Ink SuccessButtonChild(ButtonText){
     child: Container(
       height: 45,
       alignment: Alignment.center,
-      child: Text(ButtonText,style: TextStyle(
-        fontWeight: FontWeight.w400,
-        fontSize: 15
-      ),),
+      child: Text(
+        ButtonText,
+        style: TextStyle(fontWeight: FontWeight.w400, fontSize: 15),
+      ),
     ),
   );
 }
